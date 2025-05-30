4a6591058ce8a07e8dbf5757091b648ac72eaa66
Evgeny-Zhukov <ezvgeniy0011@gmail.com>
2025-05-30 14:45:06 +0300

    Выполнена лабораторная работа 7.2

---
+++ Laba7/Students/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Laba7.Models;
+
+namespace Laba7.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Laba7.Models.ApplicationContext _context;
+
+        public IndexModel(Laba7.Models.ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Students.ToListAsync();
+        }
+    }
+}
